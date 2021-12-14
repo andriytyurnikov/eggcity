f40c6899d8afe22570510f0c0b38fafc16c8e580
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2021-12-14 22:46:20 +0200

    initial responsive layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="flex flex-row justify-between gap-16 pt-4">
+  <div class="flex-initial w-1/6 md:w-1/4 lg:w-1/4">
+    <h1>EggCity Network</h1>
+  </div>
+
+  <nav class="flex-initial w-5/6 md:w-3/4 lg:w-3/4 text-right">
+    <a class="mx-2" href="//eggcity.medium.com">blog</a>
+    <a class="mx-2" href="//twitter.com/eggcity">twitter</a>
+    <a class="mx-2" href="telegram://eggcity">telegram</a>
+
+    <!-- pop-up window to send email , outlined -->
+    <a class="mx-2 border px-4 py-2 rounded-full">join the force</a>
+  </nav>
+</header>
+
+<div class="flex flex-col md:flex-row justify-between gap-8">
+  <div class="flex-initial w-full md:w-3/4">
+      <p>
+        EggCity proof of concept scans your on-chain protocol and gives you a report
+        on whether it could be qualified as the common principle DAO vehicle through a
+        zoom of diligence and compliance.
+      </p>
+  </div>
+  <div class="flex-initial w-full md:w-1/4 text-right">
+    <button class="px-4 py-2 rounded-full bg-slate-100 text-sky-900">launch scan</button>
+  </div>
+</div>
