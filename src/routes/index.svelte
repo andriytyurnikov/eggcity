ce53756006c49bd545134c1721a87a65269fc5b9
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2021-12-15 12:31:01 +0200

    initial responsive layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col justify-between min-h-full bg-sky-900 font-mono text-2xl text-slate-100 px-2 md:px-4 lg:px-16 xl:px-24">
-  <header class="flex flex-col md:flex-row justify-between pt-4 h-auto md:h-1/2">
+<div class="flex flex-col justify-between min-h-full bg-sky-900 text-slate-100 font-mono text-2xl px-2 md:px-4 lg:px-16 xl:px-24">
+  <header class="flex grow basis-1/2 flex-col md:flex-row justify-start md:justify-between pt-4 h-auto md:h-1/2">
 
     <div class="flex-initial w-full md:w-1/4 lg:w-1/4 text-center md:text-left">
       <span class="text-4xl lg:text-6xl">EggCity Network</span>
@@ -17,20 +17,22 @@
     </nav>
   </header>
 
-  <div class="flex flex-col md:flex-row justify-between">
-    <div class="flex-initial w-full md:w-2/3">
+  <main class="flex grow basis-1/2 flex-col justify-center items-center">
+    <section class="flex grow basis-full flex-col md:flex-row self-center items-center">
+      <section class="w-full md:w-2/3">
         <p>
           EggCity proof of concept scans your on-chain protocol and gives you a report
           on whether it could be qualified as the common principle DAO vehicle through a
           zoom of diligence and compliance.
         </p>
-    </div>
-    <div class="flex-initial w-full md:w-1/3 text-right">
-      <button class="mx-2 px-12 py-2 rounded-full bg-slate-100 text-sky-900 uppercase">launch scan</button>
-    </div>
-  </div>
+      </section>
+      <section class="w-full md:w-1/3 text-center md:text-right">
+        <button class="m-4 px-12 py-2 rounded-full bg-slate-100 text-sky-900 uppercase">launch scan</button>
+      </section>
+    </section>
+  </main>
 
-  <footer class="py-1 text-center text-sm">
+  <footer class="grow-0 py-1 text-center text-sm text-slate-300">
     &COPY; 2022 EggCity, All rights reserved
   </footer>
 </div>
