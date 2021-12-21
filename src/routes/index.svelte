ce90935dd1cba24c684a2168d3719909617a25ad
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2021-12-21 23:11:32 +0200

    background image composition 4h

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,21 @@
-<div class="flex flex-col justify-between min-h-full bg-sky-900 text-slate-100 font-mono text-2xl px-2 md:px-4 lg:px-16 xl:px-24">
-  <header class="flex grow basis-1/2 flex-col md:flex-row justify-start md:justify-between pt-4 h-auto md:h-1/2">
+<div class="flex flex-col justify-between h-screen font-mono text-2xl px-2 md:px-4 lg:px-16 xl:px-24">
+  <header class="header">
 
-    <div class="flex-initial w-full md:w-1/4 lg:w-1/4 text-center md:text-left">
-      <span class="text-4xl lg:text-6xl">EggCity Network</span>
+    <div class="header-brand-wrapper">
+      <span class="header-brand-item">EggCity Network</span>
     </div>
 
-    <nav class="flex-initial flex-row md:flex-col w-full md:w-3/4 lg:w-3/4 text-center md:text-right">
-      <ul class="list-none list-outside inline-block">
-        <li class="inline-block py-2"><a class="mx-2 uppercase" href="//eggcity.medium.com">blog</a></li>
-        <li class="inline-block py-2"><a class="mx-2 uppercase" href="//twitter.com/eggcity">twitter</a></li>
-        <li class="inline-block py-2"><a class="mx-2 uppercase" href="//t.me/eggcity">telegram</a></li>
+    <nav class="header-nav">
+      <ul class="header-nav-ul">
+        <li class="header-nav-li">
+          <a class="header-nav-link" href="//eggcity.medium.com">blog</a>
+        </li>
+        <li class="header-nav-li">
+          <a class="header-nav-link" href="//twitter.com/eggcity">twitter</a>
+        </li>
+        <li class="header-nav-li">
+          <a class="header-nav-link" href="//t.me/eggcity">telegram</a>
+        </li>
       </ul>
 
       <!-- pop-up window to send email , outlined -->
@@ -17,8 +23,8 @@
     </nav>
   </header>
 
-  <main class="flex grow basis-1/2 flex-col justify-center items-center">
-    <section class="flex grow basis-full flex-col md:flex-row self-center items-center">
+  <main class="main">
+    <section class="flex grow basis-full flex-col md:flex-row justify-center self-center items-center">
       <section class="w-full md:w-2/3">
         <p>
           EggCity proof of concept scans your on-chain protocol and gives you a report
@@ -32,7 +38,7 @@
     </section>
   </main>
 
-  <footer class="grow-0 py-1 text-center text-sm text-slate-300">
+  <footer class="main-section-footer">
     &COPY; 2022 EggCity, All rights reserved
   </footer>
 </div>
